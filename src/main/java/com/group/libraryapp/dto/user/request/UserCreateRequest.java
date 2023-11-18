8498344dad5f19d386b279e8b8dc46b389ddd78a
11t518s <11t518s20161759@gmail.com>
2023-11-19 00:55:28 +0900

    테스트 코드

--- src/main/java/com/group/libraryapp/dto/user/request/UserCreateRequest.java
+++ src/main/java/com/group/libraryapp/dto/user/request/UserCreateRequest.java
@@ -5,6 +5,11 @@ public class UserCreateRequest {
   private String name;
   private Integer age;
 
+  public  UserCreateRequest(String name, Integer age) {
+    this.age = age;
+    this.name = name;
+  }
+
   public String getName() {
     return name;
   }
