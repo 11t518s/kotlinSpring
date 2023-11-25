06302187d825c5b39f071ef3e76b977d9bab225d
11t518s <11t518s20161759@gmail.com>
2023-11-25 23:47:03 +0900

    test3

---
+++ src/main/kotlin/com/group/libraryapp/domain/user/loanhistory/UserLoanHistory.kt
@@ -0,0 +1,43 @@
+package com.group.libraryapp.domain.user.loanhistory
+
+import com.group.libraryapp.domain.user.User
+import javax.persistence.*
+
+@Entity
+class UserLoanHistory(
+
+    @ManyToOne
+    val user: User,
+
+    val bookName: String,
+
+    var status: UserLoanStatus = UserLoanStatus.LOANED,
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Int? = null,
+) {
+
+    fun doReturn() {
+        this.status = UserLoanStatus.RETURNED
+    }
+
+    val isReturn: Boolean
+        get() = this.status == UserLoanStatus.RETURNED
+
+    companion object {
+        fun fixture(
+            user: User,
+            bookName: String = "이상한 나라의 앨리스",
+            status: UserLoanStatus = UserLoanStatus.LOANED,
+            id: Int? = null
+        ): UserLoanHistory {
+            return UserLoanHistory(
+                user = user,
+                bookName = bookName,
+                status = status,
+                id = id,
+            )
+        }
+    }
+}
---
+++ src/main/kotlin/com/group/libraryapp/dto/user/request/UserHistoryRequest.kt
@@ -0,0 +1,5 @@
+package com.group.libraryapp.dto.user.request
+
+data class UserHistoryRequest constructor(
+    val id: Long,
+)
